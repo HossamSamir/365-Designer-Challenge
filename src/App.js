7ec303556e114ecaaa83e4cb1bb25c0f09ca9e26
Hossam Samir <Hossameos1@gmail.com>
2017-07-14 09:26:35 -0700

    basic index

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className='App'>
+        <div className='overlay'></div>
+        <div className='container'>
+          <div className='heading'>
+            <h1>#<span className='year'>365</span>-Designer-Challenge</h1>
+            <h3>Design a <b>Logo</b> for an <b>Artest</b> !</h3>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
